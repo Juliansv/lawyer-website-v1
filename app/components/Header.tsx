62d4e7e02d496027f5902e2969942b32c16e73f6
Juliansv <juliansv22@gmail.com>
2024-09-14 12:55:59 +0200

    Add toggle button for dark theme

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,16 +3,25 @@ import ToggleTheme from "./ToggleTheme";
 
 const Header = () => {
 	return (
-		<header className=" body-font dark:bg-black">
+		<header className=" body-font bg-slate-100 dark:bg-black">
 			<div className="container mx-auto flex flex-wrap md:px-32 flex-col md:flex-row items-center min-h-36 justify-center md:min-h-44">
 				<nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-					<Link href="#service" className="mr-5 hover:text-gray-500 dark:text-white dark:hover:text-slate-500">
+					<Link
+						href="#service"
+						className="mr-5 hover:text-gray-500 dark:text-white dark:hover:text-slate-500"
+					>
 						Servicios
 					</Link>
-					<Link href="#news" className="mr-5 hover:text-gray-500 dark:text-white dark:hover:text-slate-500">
+					<Link
+						href="#news"
+						className="mr-5 hover:text-gray-500 dark:text-white dark:hover:text-slate-500"
+					>
 						Noticias
 					</Link>
-					<Link href="#contact" className="hover:text-gray-500 dark:text-white dark:hover:text-slate-500">
+					<Link
+						href="#contact"
+						className="hover:text-gray-500 dark:text-white dark:hover:text-slate-500"
+					>
 						Contacto
 					</Link>
 				</nav>
@@ -32,14 +41,16 @@ const Header = () => {
 					>
 						<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
 					</svg>
-					<span className="ml-3 text-xl dark:text-white">Daniela Mateo</span>
+					<span className="ml-3 text-xl dark:text-white">
+						Daniela Mateo
+					</span>
 				</Link>
-				<div className="lg:w-2/5 lg:justify-end ml-5 lg:ml-0 hidden md:inline-flex">
-					<button className="mt-4 bg-green-500 h-10 w-28 rounded-md font-bold">
+				<div className="lg:w-2/5 lg:justify-end ml-5 lg:ml-0">
+					<ToggleTheme />
+					<button className="mt-4 bg-green-500 h-10 w-28 rounded-md font-bold text-white hidden md:inline-flex">
 						Whatsapp
 					</button>
 				</div>
-				<ToggleTheme />
 			</div>
 		</header>
 	);
--- app/components/ToggleTheme.tsx
+++ app/components/ToggleTheme.tsx
@@ -1,31 +1,41 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const ToggleTheme = () => {
-    const [darkMode, setDarkMode] = useState(true)
+	const [darkMode, setDarkMode] = useState(true);
 
-    useEffect(() => {
-        const theme = window.localStorage.getItem('theme')
-        if (theme === "dark") {
-            setDarkMode(true)
-        }
-    }, [])
+	useEffect(() => {
+		const theme = window.localStorage.getItem("theme");
+		if (theme === "dark") {
+			setDarkMode(true);
+		}
+	}, []);
 
-    useEffect(() => {
-        if (darkMode) {
-            document.documentElement.classList.add('dark')
-            window.localStorage.setItem('theme', 'dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-            window.localStorage.setItem('theme', 'light')
-        }
-    }, [darkMode])
-  return (
-    <button onClick={() => setDarkMode(!darkMode)} className="w-16 h-16 bg-slate-900 dark:bg-white rounded-full text-white dark:text-black font-semibold">
-        {darkMode ? '🌞' : '🌜'}
-    </button>
-  )
-}
+	useEffect(() => {
+		if (darkMode) {
+			document.documentElement.classList.add("dark");
+			window.localStorage.setItem("theme", "dark");
+		} else {
+			document.documentElement.classList.remove("dark");
+			window.localStorage.setItem("theme", "light");
+		}
+	}, [darkMode]);
+	return (
+		<label className="inline-flex items-center me-5 cursor-pointer">
+			<input
+				type="checkbox"
+				value=""
+				className="sr-only peer"
+				checked={darkMode}
+				onChange={() => setDarkMode(!darkMode)}
+			></input>
+			<div className="relative w-11 h-6 bg-gray-200  rounded-full peer peer-focus:ring-4 peer-focus:ring-slate-100 dark:peer-focus:ring-black dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-600"></div>
+			<span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+				
+			</span>
+		</label>
+	);
+};
 
-export default ToggleTheme
+export default ToggleTheme;
