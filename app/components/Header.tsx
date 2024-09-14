85ff6ba8ca9ce0896445aa52447930956c61e726
Juliansv <juliansv22@gmail.com>
2024-09-14 20:52:37 +0200

    fix scroll behavior

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import ToggleTheme from "./ToggleTheme";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Header = () => {
 	return (
@@ -62,55 +62,70 @@ const Links = () => {
 
 const MobileMenu = () => {
 	const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            document.documentElement.style.overflow = "hidden";
+        } else {
+            document.documentElement.style.overflow = "auto";
+        }
+    }, [isOpen]);
+
 	return (
-		<div className="md:hidden">
-			<button
-				onClick={() => {
-					setIsOpen(true);
-				}}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					height="24px"
-					viewBox="0 -960 960 960"
-					width="24px"
-					id="menu"
-					className={`${
-						isOpen && "opacity-0"
-					} cursor-pointer dark:fill-white`}
-				>
-					<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-				</svg>
-			</button>
-			<nav
-				className={`${
-					!isOpen ? "w-0 opacity-0" : "w-72 opacity-100"
-				} transition-all duration-200 flex gap-2 lg:w-2/5 flex-wrap text-base md:ml-auto flex-col absolute h-[100svh] z-30 top-0 right-0 bg-white dark:bg-black`}
-			>
-				<div className={`${!isOpen && "hidden"} flex w-full p-6`}>
-					<button onClick={() => setIsOpen(false)}>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							height="24px"
-							viewBox="0 -960 960 960"
-							width="24px"
-							id="close"
-							className="cursor-pointer dark:fill-white md:hidden"
-						>
-							<path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-						</svg>
-					</button>
-				</div>
-				<div
-					className={`${!isOpen && "hidden"} flex flex-col gap-4 items-center h-1/3 justify-evenly`}
-				>
-					<Links />
-				</div>
-				<div className={`${!isOpen && "hidden"} w-full text-center absolute bottom-10`}>
-					<ToggleTheme />
-				</div>
-			</nav>
-		</div>
+        <>
+            <div className="md:hidden z-30">
+                <button
+                    onClick={() => {
+                        setIsOpen(true);
+                    }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="24px"
+                        viewBox="0 -960 960 960"
+                        width="24px"
+                        id="menu"
+                        className={`${
+                            isOpen && "opacity-0"
+                        } cursor-pointer dark:fill-white`}
+                    >
+                        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                    </svg>
+                </button>
+                <nav
+                    className={`${
+                        !isOpen ? "w-0 opacity-0" : "w-72 opacity-100"
+                    } transition-all duration-200 flex gap-2 lg:w-2/5 flex-wrap text-base md:ml-auto flex-col absolute h-[100svh] z-30 top-0 right-0 bg-white dark:bg-black`}
+                >
+                    <div className={`${!isOpen && "hidden"} flex w-full p-6`}>
+                        <button onClick={() => setIsOpen(false)}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="24px"
+                                viewBox="0 -960 960 960"
+                                width="24px"
+                                id="close"
+                                className="cursor-pointer dark:fill-white md:hidden"
+                            >
+                                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div
+                        className={`${!isOpen && "hidden"} flex flex-col gap-4 items-center h-1/3 justify-evenly`}
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <Links />
+                    </div>
+                    <div className={`${!isOpen && "hidden"} w-full text-center absolute bottom-10`}>
+                        <ToggleTheme />
+                    </div>
+                </nav>
+            </div>
+            <div className={`${!isOpen && "hidden"} z-20 bg-slate-100 opacity-15 backdrop-blur-sm h-screen w-full absolute top-0 right-0`} onClick={() => setIsOpen(false)}>
+
+            </div>
+        </>
 	);
 };
 
