b35650b5d2f34cf3e96d1b969a065d8c38b206d4
Juliansv <juliansv22@gmail.com>
2024-09-15 18:41:04 +0200

    add whatsapp button

--- app/components/Hero-section.tsx
+++ app/components/Hero-section.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import WhatsappButton from "./Whatsapp-button";
 
 const HeroSection = () => {
 	return (
@@ -24,20 +26,9 @@ const HeroSection = () => {
 									Estudio juridico especializado en derecho
 									previsional de la seguridad social.
 								</p>
-								<a
-									href="/"
-									aria-label=""
-									className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-boctonic-acc hover:text-gray-400"
-								>
-									whatsapp
-									<svg
-										className="inline-block w-3 ml-2"
-										fill="currentColor"
-										viewBox="0 0 12 12"
-									>
-										<path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-									</svg>
-								</a>
+								<Link href="https://wa.me/0221155422323" >
+                                    <WhatsappButton />
+                                </Link>
 							</div>
 							<div className="w-full max-w-xl xl:w-5/12">
 								<div className="bg-alabaster-bg rounded shadow-2xl p-7 sm:p-10">
