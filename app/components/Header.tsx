a9c1de7bbdc4a67b45de473034609336463fd48f
Juliansv <juliansv22@gmail.com>
2024-11-09 23:36:17 +0100

    Fix navbar and slide-down animation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 
 const Header = () => {
 	return (
-		<header className="body-font bg-alabaster-bg dark:bg-wards-bg-900">
+		<header className="body-font bg-alabaster-bg dark:bg-wards-bg-900 md:px-6 lg:px-20">
 			<MobileMenu />
 			<DesktopMenu />
 		</header>
@@ -15,11 +15,11 @@ const Header = () => {
 
 const DesktopMenu = () => {
 	return (
-		<div className="hidden container mx-auto md:flex md:justify-around md:px-32">
+		<div className="hidden container md:mx-auto md:flex md:justify-between slide-down">
 			<Logo />
-            <div className="flex-grow flex gap-6 self-center pl-10 text-lg">
-			    <Links />
-            </div>
+			<div className="flex grow gap-6 self-center text-lg place-content-center absolute w-full">
+				<Links />
+			</div>
 			<ToggleTheme />
 		</div>
 	);
@@ -37,9 +37,9 @@ const MobileMenu = () => {
 	}, [isOpen]);
 
 	return (
-		<div className="container mx-auto flex flex-wrap md:hidden items-center min-h-20 justify-between px-4">
-			<Logo />
-			<div className="md:hidden z-30 max-h-6">
+		<>
+			<div className="mx-auto flex flex-wrap md:hidden items-center min-h-20 justify-between px-4 slide-down">
+				<Logo />
 				<button
 					onClick={() => {
 						setIsOpen(true);
@@ -58,49 +58,49 @@ const MobileMenu = () => {
 						<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
 					</svg>
 				</button>
-				<nav
-					className={`${
-						!isOpen ? "w-0 opacity-0" : "w-72 opacity-100"
-					} transition-all duration-200 flex gap-2 lg:w-2/5 flex-wrap text-base md:ml-auto flex-col absolute h-[100svh] z-30 top-0 right-0 bg-alabaster-bg dark:bg-wards-bg-900`}
-				>
-					<div className={`${!isOpen && "hidden"} flex w-full p-6`}>
-						<button onClick={() => setIsOpen(false)}>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								height="24px"
-								viewBox="0 -960 960 960"
-								width="24px"
-								id="close"
-								className="cursor-pointer dark:fill-porcelain-txt md:hidden"
-							>
-								<path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-							</svg>
-						</button>
-					</div>
-					<div
-						className={`${
-							!isOpen && "hidden"
-						} flex flex-col gap-4 items-center h-1/3 justify-evenly`}
-						onClick={() => setIsOpen(false)}
-					>
-						<Links />
-					</div>
-					<div
-						className={`${
-							!isOpen && "hidden"
-						} w-full text-center absolute bottom-10`}
-					>
-						<ToggleTheme />
-					</div>
-				</nav>
 			</div>
+			<nav
+				className={`${
+					!isOpen ? "w-0 opacity-0" : "w-72 opacity-100"
+				} transition-all duration-200 flex gap-2 flex-wrap text-base flex-col absolute h-[100svh] z-50 top-0 right-0 bg-alabaster-bg dark:bg-wards-bg-900`}
+			>
+				<div className={`${!isOpen ? "hidden" : ""} flex w-full p-6`}>
+					<button onClick={() => setIsOpen(false)}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							height="24px"
+							viewBox="0 -960 960 960"
+							width="24px"
+							id="close"
+							className="cursor-pointer dark:fill-porcelain-txt md:hidden"
+						>
+							<path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+						</svg>
+					</button>
+				</div>
+				<div
+					className={`${
+						!isOpen ? "hidden" : ""
+					} flex flex-col gap-4 items-center h-1/3 justify-evenly`}
+					onClick={() => setIsOpen(false)}
+				>
+					<Links />
+				</div>
+				<div
+					className={`${
+						!isOpen ? "hidden" : ""
+					} w-full text-center absolute bottom-10`}
+				>
+					<ToggleTheme />
+				</div>
+			</nav>
 			<div
 				className={`${
-					!isOpen && "hidden"
+					!isOpen ? "hidden" : ""
 				} z-20 bg-alabaster-bg opacity-15 backdrop-blur-sm h-screen w-full absolute top-0 right-0`}
 				onClick={() => setIsOpen(false)}
 			></div>
-		</div>
+		</>
 	);
 };
 
@@ -124,7 +124,9 @@ const Logo = () => {
 			</svg>
 			<div className="text-xl md:text-3xl md:w-full text-woodsmoke-txt dark:text-porcelain-txt flex flex-col">
 				Daniela Mateo
-                <span className="text-sm md:text-xl w-full leading-relaxed text-woodsmoke-txt dark:text-porcelain-txt">Abogada</span>
+				<span className="text-sm md:text-xl w-full leading-relaxed text-woodsmoke-txt dark:text-porcelain-txt">
+					Abogada
+				</span>
 			</div>
 		</Link>
 	);
@@ -154,7 +156,5 @@ const Links = () => {
 		</>
 	);
 };
-
-
 
 export default Header;
