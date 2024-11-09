824213dd3c23a675764b24fa7e98a7dd1fe24dc3
Juliansv <juliansv22@gmail.com>
2024-11-09 19:28:17 +0100

    hide unused links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -43,7 +43,7 @@ const Footer = () => {
 							<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
 						</svg>
 					</Link>
-					<a className="ml-3 text-gray-500">
+					{/* <a className="ml-3 text-gray-500">
 						<svg
 							fill="currentColor"
 							strokeLinecap="round"
@@ -92,7 +92,7 @@ const Footer = () => {
 							></path>
 							<circle cx="4" cy="4" r="2" stroke="none"></circle>
 						</svg>
-					</a>
+					</a> */}
 				</span>
 			</div>
 		</footer>
