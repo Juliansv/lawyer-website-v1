cddb3527437a59eb970b0ecbd1403e002a2565dd
Juliansv <juliansv22@gmail.com>
2024-11-11 13:44:26 +0100

    Change svg icons

---
+++ app/components/Icons.tsx
@@ -0,0 +1,440 @@
+export const LawyerIcon = () => (
+	<svg
+		width="150px"
+		height="150px"
+		viewBox="0 0 1024 1024"
+		className="icon max-h-full p-1"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			<path
+				d="M512 616.2m-10 0a10 10 0 1 0 20 0 10 10 0 1 0-20 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M511.6 656.9m-10 0a10 10 0 1 0 20 0 10 10 0 1 0-20 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M512.4 697.7m-10 0a10 10 0 1 0 20 0 10 10 0 1 0-20 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M512 130.8c42.1 0 81.7 16.4 111.5 46.2s46.2 69.4 46.2 111.5-16.4 81.7-46.2 111.5c-29.8 29.8-69.4 46.2-111.5 46.2s-81.7-16.4-111.5-46.2c-29.8-29.8-46.2-69.4-46.2-111.5s16.4-81.7 46.2-111.5 69.4-46.2 111.5-46.2m0-44c-111.4 0-201.6 90.3-201.6 201.6C310.4 399.8 400.7 490 512 490c111.4 0 201.6-90.3 201.6-201.6S623.3 86.8 512 86.8zM512.3 523.5L84 681.4v255.7h856V681.4L512.3 523.5zM896 893.1H128V712.6l384.3-142.4L896 712.6v180.5z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M555.4 585.3l-1.4-0.5v159.9c0 11.7-4.8 22.3-12.4 30-7.7 7.7-18.3 12.4-30 12.4-23.4 0-42.4-19-42.4-42.4V585.3l-1.4 0.5-14.6 5.2v153.8c0 32.2 26.2 58.4 58.4 58.4S570 777 570 744.8V590.5l-14.6-5.2z"
+				fill="#3A3BCD"
+			></path>
+		</g>
+	</svg>
+);
+
+export const BriefcaseIcon = () => (
+	<svg
+		width="150px"
+		height="150px"
+		viewBox="0 0 1024 1024"
+		className="icon max-h-full p-1"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			<path
+				d="M512 569.2m-10 0a10 10 0 1 0 20 0 10 10 0 1 0-20 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M611.8 617.1H406.5L108 390.3v20.1l293 222.7h216l299-223.3v-20L611.8 617.1z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M704 256.8v-94.5H320v94.5H64v605h896v-605H704z m-340-50.5h296v50.4H364v-50.4z m552 611.5H108V769h808v48.8z m0-64.9H108V300.8h808v452.1z"
+				fill="#3A3BCD"
+			></path>
+		</g>
+	</svg>
+);
+
+export const CalculatorIcon = () => (
+	<svg
+		width="150px"
+		height="150px"
+		viewBox="0 0 1024 1024"
+		className="icon max-h-full p-1"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			<path d="M928.1 881v44H95.9V99h44v782z" fill="#3A3BCD"></path>
+			<path
+				d="M352 435.7v403.4H204V435.7h148m22-22H182v447.4h192V413.7zM608 307.9v531.2H460V307.9h148m22-22H438v575.2h192V285.9z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M866.1 177.3v663.9H714V177.3h152.1m20-20H694v703.9h192V157.3h0.1z"
+				fill="#3A3BCD"
+			></path>
+		</g>
+	</svg>
+);
+
+export const PlanningIcon = () => (
+	<svg
+		width="150px"
+		height="150px"
+		viewBox="0 0 1024 1024"
+		className="icon max-h-full p-1"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			<path
+				d="M716 190.9v-67.8h-44v67.8H352v-67.8h-44v67.8H92v710h840v-710H716z m-580 44h172v69.2h44v-69.2h320v69.2h44v-69.2h172v151.3H136V234.9z m752 622H136V402.2h752v454.7z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M319 565.7m-33 0a33 33 0 1 0 66 0 33 33 0 1 0-66 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M510 565.7m-33 0a33 33 0 1 0 66 0 33 33 0 1 0-66 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M701.1 565.7m-33 0a33 33 0 1 0 66 0 33 33 0 1 0-66 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M319 693.4m-33 0a33 33 0 1 0 66 0 33 33 0 1 0-66 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M510 693.4m-33 0a33 33 0 1 0 66 0 33 33 0 1 0-66 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M701.1 693.4m-33 0a33 33 0 1 0 66 0 33 33 0 1 0-66 0Z"
+				fill="#3A3BCD"
+			></path>
+		</g>
+	</svg>
+);
+
+export const UpdateIcon = () => (
+	<svg
+		width="150px"
+		height="150px"
+		viewBox="0 0 1024 1024"
+		className="icon max-h-full p-1"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			<path
+				d="M719.8 651.8m-10 0a10 10 0 1 0 20 0 10 10 0 1 0-20 0Z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M512.1 64H172v896h680V385.6L512.1 64z m278.8 324.3h-280v-265l280 265zM808 916H216V108h278.6l0.2 0.2v296.2h312.9l0.2 0.2V916z"
+				fill="#3A3BCD"
+			></path>
+			<path d="M280.5 530h325.9v16H280.5z" fill="#3A3BCD"></path>
+			<path d="M639.5 530h90.2v16h-90.2z" fill="#3A3BCD"></path>
+			<path d="M403.5 641.8h277v16h-277z" fill="#3A3BCD"></path>
+			<path d="M280.6 641.8h91.2v16h-91.2z" fill="#3A3BCD"></path>
+			<path d="M279.9 753.7h326.5v16H279.9z" fill="#3A3BCD"></path>
+			<path d="M655.8 753.7h73.9v16h-73.9z" fill="#3A3BCD"></path>
+		</g>
+	</svg>
+);
+
+export const SupportIcon = () => (
+	<svg
+		width="150px"
+		height="150px"
+		viewBox="0 0 1024 1024"
+		className="icon max-h-full p-1"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			<path
+				d="M844.1 211.4c-3.6-4-7.3-7.9-11-11.7-53.8-55.4-121.9-96.9-198.2-118.7-15.1-4.3-30.5-7.8-46.2-10.5-25-4.3-50.6-6.5-76.7-6.5-126 0-239.9 52-321.3 135.8-3.7 3.8-7.4 7.7-11 11.7C107.8 290.9 64 396.4 64 512s43.8 220.9 115.6 300.4c3.6 4 7.2 7.9 11 11.7 53.9 55.5 122.1 97.1 198.6 118.9 15.1 4.3 30.5 7.8 46.2 10.5 24.9 4.3 50.5 6.5 76.6 6.5s51.7-2.2 76.7-6.5c15.7-2.7 31.1-6.2 46.2-10.5 76.5-21.7 144.6-63.3 198.5-118.8 3.7-3.9 7.4-7.7 11-11.7C916.2 733 960 627.6 960 512c0-115.7-43.9-221.1-115.9-300.6z m71.8 308.8c-1 51.7-11.6 101.8-31.6 149.1-17.4 41.2-41.3 78.8-71.1 112-3.5 4-7.2 7.9-10.9 11.7-1.5 1.6-3.1 3.1-4.6 4.7-37.1 37.1-80.3 66.3-128.4 86.6-35.8 15.1-73.1 24.9-111.6 29.2-12.5 1.4-25 2.2-37.7 2.4-2.6 0.1-5.3 0.1-8 0.1s-5.3 0-8-0.1c-12.6-0.2-25.2-1-37.7-2.4-38.4-4.3-75.8-14.1-111.6-29.2-48.1-20.4-91.3-49.5-128.4-86.6-1.6-1.6-3.2-3.2-4.7-4.8-3.7-3.8-7.4-7.8-10.9-11.7-29.8-33.2-53.6-70.8-71-111.9-20-47.3-30.6-97.4-31.6-149.1h109.3c0-2.7-0.1-5.4-0.1-8.2 0-2.6 0-5.2 0.1-7.8H108.1c1-51.8 11.6-102 31.7-149.4 17.4-41.2 41.3-78.9 71.2-112.1 3.5-4 7.2-7.9 10.9-11.7 1.5-1.5 3-3.1 4.5-4.6 37.1-37.1 80.3-66.3 128.4-86.6 35.8-15.1 73.1-24.9 111.6-29.2 12.5-1.4 25-2.2 37.7-2.5 2.7-0.1 5.3-0.1 8-0.1s5.3 0 8 0.1c12.7 0.2 25.2 1.1 37.7 2.5 38.4 4.3 75.8 14.1 111.5 29.2 48.1 20.3 91.3 49.5 128.4 86.6 1.5 1.5 3 3 4.4 4.5 3.7 3.8 7.4 7.7 10.9 11.7 29.9 33.3 53.8 70.9 71.3 112.2 20 47.4 30.7 97.6 31.6 149.4H806.6c0 2.6 0.1 5.2 0.1 7.8 0 2.7 0 5.4-0.1 8.2h109.3z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M790.7 512c0 2.7 0 5.4-0.1 8.2H520v144.7c-2.7-0.1-5.4-0.1-8.1-0.1-2.6 0-5.3 0-7.9 0.1V520.2H233.4c-0.1-2.7-0.1-5.4-0.1-8.2 0-2.6 0-5.2 0.1-7.8H504V358.8c2.6 0 5.3 0.1 7.9 0.1 2.7 0 5.4 0 8.1-0.1v145.4h270.6c0 2.6 0.1 5.2 0.1 7.8zM520 111.1v231.6c-2.7 0.1-5.4 0.1-8.1 0.1-2.6 0-5.3 0-7.9-0.1V111.1c2.7-1.1 5.3-2.1 8-3.1 2.7 1 5.3 2.1 8 3.1zM512 916zM520 680.9v232c-2.7 1.1-5.3 2.1-8 3.1-2.7-1-5.3-2.1-8-3.1v-232c2.6-0.1 5.3-0.1 7.9-0.1 2.7 0 5.4 0 8.1 0.1z"
+				fill="#3A3BCD"
+			></path>
+			<path
+				d="M512 916zM512 916zM748.7 732.5c35.6-62.9 56.5-135.2 57.8-212.3 0-2.7 0.1-5.4 0.1-8.2 0-2.6 0-5.2-0.1-7.8-1.3-77.3-22.2-149.9-58-212.9 23-14.3 44.5-30.6 64.4-48.7-3.6-4-7.2-7.9-10.9-11.7-19.2 17.5-39.8 33.1-61.7 46.7-43.9-71.4-107.3-129.5-182.8-167-12.5-1.4-25-2.2-37.7-2.5-2.7-0.1-5.3-0.1-8-0.1 2.7 1 5.3 2.1 8 3.1 2.3 0.9 4.5 1.9 6.8 2.8 51.4 21.8 97.6 52.9 137.3 92.6 24.1 24.1 45 50.6 62.7 79.2-15 8.6-30.6 16.4-46.7 23.2-50.8 21.5-104.6 32.9-160 33.9-2.7 0.1-5.4 0.1-8.1 0.1-2.6 0-5.3 0-7.9-0.1-55.5-1-109.4-12.4-160.3-33.9-16-6.8-31.6-14.5-46.6-23.1 17.6-28.6 38.6-55.2 62.7-79.3 39.7-39.7 85.9-70.8 137.3-92.6 2.3-0.9 4.5-1.9 6.8-2.8 2.7-1.1 5.3-2.1 8-3.1-2.7 0-5.3 0-8 0.1-12.7 0.2-25.2 1.1-37.7 2.5-75.5 37.5-138.9 95.6-182.8 167.1-21.9-13.6-42.5-29.2-61.7-46.7-3.7 3.8-7.4 7.8-10.9 11.7 19.9 18.1 41.5 34.4 64.5 48.7-35.7 63-56.6 135.5-57.9 212.8 0 2.6-0.1 5.2-0.1 7.8 0 2.7 0 5.4 0.1 8.2 1.4 77 22.2 149.3 57.8 212.2-23 14.3-44.6 30.7-64.5 48.8 3.5 4 7.2 7.9 10.9 11.7 19.2-17.5 39.8-33.1 61.7-46.8 43.9 71.6 107.4 129.8 183 167.4 12.5 1.4 25 2.2 37.7 2.4 2.7 0.1 5.3 0.1 8 0.1-2.7-1-5.3-2.1-8-3.1-2.3-0.9-4.5-1.9-6.8-2.8-51.4-21.8-97.6-52.9-137.3-92.6-24.2-24.2-45.2-50.8-62.8-79.5 15-8.6 30.6-16.4 46.7-23.2 50.8-21.5 104.7-32.9 160.3-33.9 2.6-0.1 5.3-0.1 7.9-0.1 2.7 0 5.4 0 8.1 0.1 55.5 1 109.3 12.4 160 33.9 16.2 6.8 31.8 14.6 46.9 23.3-17.6 28.7-38.6 55.3-62.8 79.5-39.7 39.7-85.9 70.8-137.3 92.6-2.3 1-4.5 1.9-6.8 2.8-2.7 1.1-5.3 2.1-8 3.1 2.7 0 5.3 0 8-0.1 12.7-0.2 25.2-1 37.7-2.4 75.6-37.5 139-95.7 182.9-167.3 21.8 13.6 42.5 29.3 61.7 46.8 3.7-3.8 7.3-7.7 10.9-11.7-19.9-18.3-41.5-34.6-64.5-48.9zM520 664.9c-2.7-0.1-5.4-0.1-8.1-0.1-2.6 0-5.3 0-7.9 0.1-78.2 1.4-151.5 22.8-215.1 59.3-8-14.2-15.2-28.9-21.6-44-21.5-50.8-32.9-104.5-33.9-160-0.1-2.7-0.1-5.4-0.1-8.2 0-2.6 0-5.2 0.1-7.8 1-55.6 12.4-109.5 33.9-160.3 6.4-15.2 13.7-30 21.8-44.3 63.5 36.5 136.8 57.9 214.9 59.2 2.6 0 5.3 0.1 7.9 0.1 2.7 0 5.4 0 8.1-0.1 78.1-1.4 151.4-22.8 214.9-59.3 8.1 14.3 15.4 29.1 21.8 44.4 21.5 50.9 32.9 104.8 33.9 160.3 0 2.6 0.1 5.2 0.1 7.8 0 2.7 0 5.4-0.1 8.2-1 55.4-12.4 109.2-33.9 160-6.4 15.2-13.6 29.9-21.7 44.1-63.5-36.6-136.8-58-215-59.4z"
+				fill="#3A3BCD"
+			></path>
+		</g>
+	</svg>
+);
+
+export const HammerIcon = () => (
+	<svg
+		viewBox="0 -0.12 44.78 44.78"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			{" "}
+			<g transform="translate(-728.199 -32.31)">
+				{" "}
+				<path
+					d="M750.622,68.36h18.964a1.5,1.5,0,0,1,.891,1.117v2.305c0,.466-.654.578-.891.578H750.622c-.237,0-.145-.112-.145-.578V69.477C750.477,69.012,750.385,68.36,750.622,68.36Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<path
+					d="M747.982,72.36h24.244c.3,0,.251.635.251,1.091v2.257c0,.456.052.652-.251.652H747.982c-.3,0-.505-.2-.505-.652V73.451C747.477,73,747.678,72.36,747.982,72.36Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<path
+					d="M738.629,46.481l13.595-13.595c.17-.17.6-.021.95.334l1.757,1.757c.354.354.5.78.334.95l-13.6,13.6c-.17.17-.595.02-.95-.334l-1.757-1.757C738.609,47.076,738.459,46.651,738.629,46.481Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<path
+					d="M752.58,60.432l13.595-13.595c.17-.17.6-.021.95.334l1.757,1.757c.354.354.5.78.334.95l-13.6,13.6c-.17.17-.6.02-.95-.335l-1.757-1.756C752.559,61.027,752.41,60.6,752.58,60.432Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<rect
+					width="16.446"
+					height="15.429"
+					transform="translate(742.653 48.539) rotate(-45)"
+					stroke-width="1"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					fill="none"
+				></rect>{" "}
+				<path
+					d="M2.649,0H22.972a0,0,0,0,1,0,0V5.3a0,0,0,0,1,0,0H2.649A2.649,2.649,0,0,1,0,2.649v0A2.649,2.649,0,0,1,2.649,0Z"
+					transform="translate(728.906 67.28) rotate(-45)"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+			</g>{" "}
+		</g>
+	</svg>
+);
+
+export const ClockIcon = () => (
+	<svg
+		viewBox="-7.5 0 42 42"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			{" "}
+			<g transform="translate(-930.977 -120.86)">
+				{" "}
+				<path
+					d="M932,121.36h24.911c.312,0,.565.367.565.835v2.318c0,.468-.253.847-.565.847H932c-.311,0-.524-.379-.524-.847v-2.318C931.477,121.727,931.69,121.36,932,121.36Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<path
+					d="M932,162.36h24.911c.312,0,.565-.41.565-.872V159.2c0-.462-.253-.837-.565-.837H932c-.311,0-.524.375-.524.837v2.291C931.477,161.95,931.69,162.36,932,162.36Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<path
+					d="M955.477,128.334V125.36h-22v3.115a13.048,13.048,0,0,0,5.021,10.263l3.832,2.968-3.843,2.968a13.041,13.041,0,0,0-5.01,10.264v3.422h22v-3.281a12.791,12.791,0,0,0-4.923-10.4l-3.842-2.974,3.785-2.973A12.846,12.846,0,0,0,955.477,128.334Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<path
+					d="M944.893,148.442c-.006.005,0-.005,0,0s.006.08,0,.075a20.213,20.213,0,0,0-8.792,9.843h17.181A20.972,20.972,0,0,0,944.893,148.442Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+				<path
+					d="M944.477,132.36H937.47a12.623,12.623,0,0,0,7,6.266l0,0,0,0a12.623,12.623,0,0,0,7-6.266Z"
+					fill="none"
+					stroke="#3A3BCD"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="1"
+				></path>{" "}
+			</g>{" "}
+		</g>
+	</svg>
+);
+
+export const BalanceIcon = () => (
+	<svg
+		viewBox="0 -0.55 46.439 46.439"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="#000000"
+	>
+		<g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+		<g
+			id="SVGRepo_tracerCarrier"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		></g>
+		<g id="SVGRepo_iconCarrier">
+			{" "}
+			<g transform="translate(-824.507 -32.514)">
+				{" "}
+				<path
+					d="M849.477,73.36h-3V34.831a1.644,1.644,0,0,1,1.6-1.817h-.209a1.65,1.65,0,0,1,1.609,1.817Z"
+					fill="none"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1"
+				></path>{" "}
+				<line
+					x2="12"
+					transform="translate(833.477 40.36)"
+					strokeWidth="1"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					fill="none"
+				></line>{" "}
+				<line
+					x2="11"
+					transform="translate(850.477 40.36)"
+					strokeWidth="1"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					fill="none"
+				></line>{" "}
+				<path
+					d="M834,73.36h27.727c.346,0,.753.781.753,1.161V76.4c0,.38-.407.96-.753.96H834c-.347,0-.52-.58-.52-.96V74.521C833.477,74.141,833.65,73.36,834,73.36Z"
+					fill="none"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1"
+				></path>{" "}
+				<path
+					d="M825.164,59.36l8.188-19,6.438,19"
+					fill="none"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1"
+				></path>{" "}
+				<path
+					d="M832.477,66.3a7.024,7.024,0,0,0,7.312-6.936H825.164A7.025,7.025,0,0,0,832.477,66.3Z"
+					fill="none"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1"
+				></path>{" "}
+				<path
+					d="M870.289,59.36l-8.187-19-6.437,19"
+					fill="none"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1"
+				></path>{" "}
+				<path
+					d="M862.977,66.3a7.025,7.025,0,0,1-7.313-6.936h14.625A7.024,7.024,0,0,1,862.977,66.3Z"
+					fill="none"
+					stroke="#3A3BCD"
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1"
+				></path>{" "}
+			</g>{" "}
+		</g>
+	</svg>
+);
